d0ee3bbad889462ca8ca066873ce118f8705663d
Alok Aryal <91200494+Saysan777@users.noreply.github.com>
2023-02-28 13:20:46 +0545

    misc

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,7 +17,8 @@ const Hero = () => (
       </SectionTitle>
       <SectionText>
         Hello, My name is <strong>Alok aryal</strong> and i completed my Bachelors in
-        CSIT from ACHS college. I'm currently working as a full-stack software developer in <a href="https://www.keela.co/about-us" style={{ text-decoration: none }}> Keela </a>. I definitely do
+        CSIT from ACHS college. I'm currently working as a full-stack software developer in 
+        <a href="https://www.keela.co/about-us" style={{ textDecoration: none}}> Keela </a>. I definitely do
         love the idea of coding so have been crafting and managing initutaive web applications.
       </SectionText>
       <Button>
